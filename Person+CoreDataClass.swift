b9e7704146de7c385c48083d7a711f5e5ec12640
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2020-01-16 01:16:46 +0600

    Created Entity and Attributes

---
+++ Person+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Person+CoreDataClass.swift
+//  CoreData CRUD using Storyboard
+//
+//  Created by Md Khaled Hasan Manna on 16/1/20.
+//  Copyright © 2020 Md Khaled Hasan Manna. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
---
+++ Person+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+//
+//  Person+CoreDataProperties.swift
+//  CoreData CRUD using Storyboard
+//
+//  Created by Md Khaled Hasan Manna on 16/1/20.
+//  Copyright © 2020 Md Khaled Hasan Manna. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Person {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Person> {
+        return NSFetchRequest<Person>(entityName: "Person")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var city: String?
+
+}
